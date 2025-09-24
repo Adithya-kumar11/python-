2ee6dce46d63510a5bd5a5fa550a8a142009bafe
Adithya-kumar11 <adithyakonjerla@gmail.com>
2025-09-24 23:18:35 +0530

    adding five codes

---
+++ primenumbers.py
@@ -0,0 +1,14 @@
+def primes_up_to_n(n):
+    primes = []
+    for num in range(2, n + 1):
+        is_prime = True
+        for i in range(2, int(num ** 0.5) + 1):
+            if num % i == 0:
+                is_prime = False
+                break
+        if is_prime:
+            primes.append(num)
+    return primes
+
+n = int(input("Enter a number: "))
+print(f"Prime numbers up to {n}: {primes_up_to_n(n)}")
